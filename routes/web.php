6c69d80fad53fa3c932ad5f022c39dd2bd3f7587
Michaellim2000 <michael18001@mail.unpad.ac.id>
2020-08-15 16:16:21 +0700

    Bagianku

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
+
+Auth::routes();
+
 Route::get('/', function () {
-    return view('welcome');
+    // return view('item.index');
+    return view('auth.login');
+});
+Route::group(['middleware'=>'auth'],function(){
+//Route::get('/', 'HomeController@index')->name('home');
+Route::get('/pertanyaan','pertanyaanController@index')->name('pertanyaan.index');
+Route::get('/pertanyaan/create','pertanyaanController@create')->name('pertanyaan.create');
+Route::post('/pertanyaanc','pertanyaanController@store');
+// Route::post('/pertanyaanvu','pertanyaanController@pertanyaanvoteu');
+// Route::post('/pertanyaanvd','pertanyaanController@pertanyaanvoted');
+Route::get('/pertanyaan/{id}','pertanyaanController@show')->name('pertanyaan.show');
+Route::get('/pertanyaan/{id}/edit','pertanyaanController@edit');
+Route::put('/pertanyaan/{id}','pertanyaanController@update');
+Route::delete('/pertanyaan/{id}','pertanyaanController@destroy');
+
+Route::get('/jawaban/{id}','jawabanController@index')->name('jawaban.index');
+Route::get('/jawaban/create/{id}','jawabanController@create');
+Route::post('/jawaban/{id}','jawabanController@store')->name('jawaban.store');
+// // Route::post('/pertanyaanvu','pertanyaanController@pertanyaanvoteu');
+// Route::post('/pertanyaanvd','pertanyaanController@pertanyaanvoted');
+// Route::get('/jawaban/{id}','jawabanController@show');
+Route::get('/jawaban/{id}/edit','jawabanController@edit');
+Route::put('/jawaban/{id}','jawabanController@update');
+Route::delete('/jawaban/{id}','jawabanController@destroy');
+
+
+Route::get('/komentar_pertanyaan/{id}', 'KomentarController@pertanyaan')->name('komentar.pertanyaan');
+Route::post('/komentar_pertanyaan/{id}', 'KomentarController@store')->name('komentar.store');
+Route::get('/komentar_jawaban/{id}', 'KomentarController@jawaban')->name('komentar.jawaban');
+Route::post('/komentar_jawaban/{id}', 'KomentarController@store')->name('komentar.jawaban.store');
+
+Route::get('/vote_pertanyaan_up/{id}', 'VoteController@pertanyaan_up')->name('votepertanyaan.up');
+Route::get('/vote_pertanyaan_down/{id}', 'VoteController@pertanyaan_down')->name('votepertanyaan.down');
+Route::get('/vote_jawaban_up/{id}', 'VoteController@jawaban_up')->name('votejawaban.up');
+Route::get('/vote_jawaban_down/{id}', 'VoteController@jawaban_down')->name('votejawaban.down');
+
+Route::get('/vote_jawaban_terbaik/{id}', 'VoteController@best_answer')->name('vote.best');
+
+
 });
