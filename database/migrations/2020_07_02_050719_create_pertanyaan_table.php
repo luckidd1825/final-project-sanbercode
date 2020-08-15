97f93f63e6cc2d097ba211f61563688ee5b72bef
Michaellim2000 <michael18001@mail.unpad.ac.id>
2020-08-15 18:54:14 +0700

    Bagian Database

--- database/migrations/2020_07_02_050719_create_pertanyaan_table.php
+++ database/migrations/2020_07_02_050719_create_pertanyaan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePertanyaanTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pertanyaan', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('reputasi')->nullable();
-            $table->rememberToken();
+            $table->string('judul');
+            $table->string('isi');
+            $table->integer('poinvote')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users'); 
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pertanyaan');
     }
 }
